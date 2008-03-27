35ce91178048d3766620e5c6cee88d6c183578f4
Olivier Ruffin <olivier@veilleperso.com>
2008-03-27 14:08:46 +0100

    BF

--- or_target_blank.php
+++ or_target_blank.php
@@ -8,21 +8,16 @@
   Author URI: http://veilleperso.com
 */
 
-
-define('OR_EXTERNAL_LINK', "/^".preg_quote(get_option("siteurl"), "/")."/");
+define('OR_EXTERNAL_LINK', "/^".preg_quote(get_option("siteurl"), "/i")."/");
 
 function or_convert_external_link($matches) {
-  if (preg_match(OR_EXTERNAL_LINK, $matches[2])) return '<a href="'.$matches[2].'"'.$matches[1].$matches[3].">";
-  else return '<a href="'.$matches[2].'"'.$matches[1].$matches[3]." target='_blank'>";
+  if (preg_match(OR_EXTERNAL_LINK, $matches[2])) return "<a href=\"$matches[2]\"$matches[1]$matches[3]>";
+  else return "<a href=\"$matches[2]\"$matches[1]$matches[3] target=\"_blank\">";
 }
 	
-
 function or_external_link($text) {
-  //use a target blank on external links
-	// Comment text in popup windows should be filtered through this.
-	// Right now it's a moderately dumb function, ideally it would detect whether
-	// a target or rel attribute was already there and adjust its actions accordingly.
-	$pattern = '/<a(.*?)href=[\'"](https*:\/\/.*?)[\'"](.*?)>/i';
+  // use a target blank on external links
+	$pattern = '/<a(.*?)href=[\"\'](https*:\/\/.*?)[\"\'](.*?)>/i';
 	return preg_replace_callback($pattern, 'or_convert_external_link', $text);
 }
 
