1ff3828c5fcf548e1aed9e871939f28b76d48f82
Olivier Ruffin <olivier@veilleperso.com>
2008-04-09 16:34:50 +0200

    Bug fix pattern matching

--- or_target_blank.php
+++ or_target_blank.php
@@ -3,24 +3,26 @@
   Plugin Name: Target Blank
   Plugin Author: Olivier Ruffin
   Description: A Wordpress plugin to add a target='_blank' to all post and comments links which are external to the blog
-  Version: 1.0
+  Version: 1.1
   Author: Olivier Ruffin
   Author URI: http://veilleperso.com
 */
 
-define('OR_EXTERNAL_LINK', "/^".preg_quote(get_option("siteurl"), "/i")."/");
+define('OR_INTERNAL_LINK', "/^".preg_quote(get_option("siteurl"), "/i")."/");
 
 function or_convert_external_link($matches) {
-  if (preg_match(OR_EXTERNAL_LINK, $matches[2])) return "<a href=\"$matches[2]\"$matches[1]$matches[3]>";
+  if (preg_match(OR_INTERNAL_LINK, $matches[2])) return "<a href=\"$matches[2]\"$matches[1]$matches[3]>";
   else return "<a href=\"$matches[2]\"$matches[1]$matches[3] target=\"_blank\">";
 }
 	
 function or_external_link($text) {
   // use a target blank on external links
-	$pattern = '/<a(.*?)href=[\"\'](https*:\/\/.*?)[\"\'](.*?)>/i';
+	$pattern = '/<a([^>]*?)href=[\"\'](https*:\/\/.*?)[\"\']([^>]*?)>/i';
 	return preg_replace_callback($pattern, 'or_convert_external_link', $text);
 }
 
-add_filter('comment_text', 'or_external_link', 999);
-add_filter('the_content', 'or_external_link', 999);
-add_filter('get_comment_author_link', 'or_external_link', 999);
+if (!is_feed()) {
+  add_filter('comment_text', 'or_external_link', 999);
+  add_filter('the_content', 'or_external_link', 999);
+  add_filter('get_comment_author_link', 'or_external_link', 999);
+}
